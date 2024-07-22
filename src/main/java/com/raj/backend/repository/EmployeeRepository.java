37b517e6b885123c2a670c35284ac66b15537135
Raj Kumar <119596365+kumar-modukuri@users.noreply.github.com>
2024-07-22 16:30:36 +0530

    Delete src directory

--- src/main/java/com/raj/backend/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.raj.backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.raj.backend.model.Employee;
-
-public interface EmployeeRepository extends MongoRepository<Employee,Integer>
-{
-	
-}
